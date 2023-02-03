d1dc33721d1884b64e5f90307c0e534dbee0d3a1
simmigyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-03 10:35:34 +0900

    [Feat] 프로젝트 초기 세팅

---
+++ src/main/java/dnd/diary/DiaryApplication.java
@@ -0,0 +1,13 @@
+package dnd.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dnd/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dnd.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
