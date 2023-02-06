5b07125ad2a9d1438afa77bb58aba52f2cfadd3f
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-07 05:45:53 +0900

    Update DiaryApplication.java

--- src/main/java/dnd/diary/DiaryApplication.java
+++ src/main/java/dnd/diary/DiaryApplication.java
@@ -3,7 +3,8 @@ package dnd.diary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DiaryApplication {
 
 	public static void main(String[] args) {
