5579a8fa89ddc152f308947cd873474f8af0ad2d
simmigyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-08 10:16:51 +0900

    [Build] 배포 스크립트 수정 - Nginx 설정 변경
    - 배포 시 health check 실패로 Nginx 설정 제외하도록 수정

---
+++ scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/deploy
+
+echo">check running pid"
+
+CURRENT_PID=$(pgrep -f dnd-8th-4-backend)
+
+echo "> CURRENT_PID"
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> There is no pid running currently."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> deploy new application"
+
+echo "> copy build file"
+
+cp $REPOSITORY/*.jar $REPOSITORY/jar/
+
+JAR_NAME=$(ls -tr $REPOSITORY/jar/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> give authority to $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/deploy/application-dev.yml \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
