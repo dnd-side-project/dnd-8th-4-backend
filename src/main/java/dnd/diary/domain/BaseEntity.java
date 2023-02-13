15f00592c01136491e80d1455f86b8abfaffd89e
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-14 01:00:32 +0900

    [Feat] ] BaseEntity 클래스 생성 및 JpaAuditing 설정 추가

---
+++ src/main/java/dnd/diary/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package dnd.diary.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
