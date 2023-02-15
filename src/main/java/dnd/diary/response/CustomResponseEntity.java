44c02076ee3a79123b43618c86db95588cd0fdb6
simmigyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-15 13:17:45 +0900

    [Feat] 공통 응답, 예외 처리 클래스 추가

---
+++ src/main/java/dnd/diary/response/CustomResponseEntity.java
@@ -0,0 +1,35 @@
+package dnd.diary.response;
+
+import dnd.diary.enumeration.Result;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CustomResponseEntity<T> {
+
+	private int code;
+	private String message;
+	private T data;
+
+	public static <T> CustomResponseEntity<T> success(T data) {
+		return CustomResponseEntity.<T>builder()
+			.code(Result.OK.getCode())
+			.message(Result.OK.getMessage())
+			.data(data)
+			.build();
+	}
+
+	public static <T> CustomResponseEntity<T> fail(Result result) {
+		return CustomResponseEntity.<T>builder()
+			.code(result.getCode())
+			.message(result.getMessage())
+			.build();
+	}
+
+	@Builder
+	public CustomResponseEntity(int code, String message, T data) {
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
+}
