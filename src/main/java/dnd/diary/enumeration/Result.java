8bcc3c0497f261128ff87326f2f838ebf15324fc
simmigyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-17 16:39:20 +0900

    [Feat] S3 관련 설정 추가 및 이미지 업로드/삭제 로직 추가

--- src/main/java/dnd/diary/enumeration/Result.java
+++ src/main/java/dnd/diary/enumeration/Result.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 public enum Result {
 
 	OK(0, "성공"),
-	FAIL(-1, "실패");
+	FAIL(-1, "실패"),
+	FAIL_IMAGE_UPLOAD(2000, "파일 업로드 실패"),
+	NOT_FOUND_USER(2100, "존재하지 않는 사용자"),
+	NOT_FOUND_GROUP(2101, "존재하지 않는 그룹"),
+	LOW_MIN_GROUP_NAME_LENGTH(2102, "그룹 이름 최소 글자(1자) 미만"),
+	HIGH_MAX_GROUP_NAME_LENGTH(2103, "그룹 이름 최대 글자(12자) 초과"),
+	NO_USER_GROUP_LIST(2104, "가입한 그룹이 없는 경우");
 
 	private final int code;
 	private final String message;
---
+++ src/main/java/dnd/diary/service/s3/S3Service.java
@@ -0,0 +1,78 @@
+package dnd.diary.service.s3;
+
+import static dnd.diary.enumeration.Result.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import dnd.diary.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	private final AmazonS3 amazonS3;
+
+	public List<String> upload(List<MultipartFile> multipartFile) {
+
+		List<String> fileUrlList = new ArrayList<>();
+
+		multipartFile.forEach(file -> {
+			String fileName = createFileName(file.getOriginalFilename());
+			ObjectMetadata objectMetadata = new ObjectMetadata();
+			objectMetadata.setContentLength(file.getSize());
+			objectMetadata.setContentType(file.getContentType());
+
+			try (InputStream inputStream = file.getInputStream()) {
+				amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+					.withCannedAcl(CannedAccessControlList.PublicRead));
+				String fileUrl = amazonS3.getUrl(bucket, fileName).toString();
+				fileUrlList.add(fileUrl);
+			} catch (IOException e) {
+				throw new CustomException(FAIL_IMAGE_UPLOAD);
+			}
+
+		});
+
+		return fileUrlList;
+	}
+
+	private String createFileName(String fileName) {
+		// 랜덤으로 파일 이름 생성
+		return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+	}
+
+	// 파일 확장자 전달
+	private String getFileExtension(String fileName) {
+		try {
+			return fileName.substring(fileName.lastIndexOf("."));
+		} catch (StringIndexOutOfBoundsException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일 (" + fileName + ") 입니다.");
+		}
+	}
+
+	public void remove(String fileUrl) throws Exception {
+		String fileName = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
+		amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
+	}
+
+}
