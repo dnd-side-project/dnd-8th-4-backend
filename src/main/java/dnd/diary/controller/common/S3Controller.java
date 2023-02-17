a73c8b84ac096bbe6df14ccd1c94e0aeaccfaf0c
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-18 00:30:48 +0900

    Revert "[Feat] RESTDocs 관련 디렉토리 오류 수정"

--- src/main/java/dnd/diary/controller/common/S3Controller.java
+++ src/main/java/dnd/diary/controller/common/S3Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import dnd.diary.response.CustomResponseEntity;
+import dnd.diary.response.common.CustomResponseEntity;
 import dnd.diary.service.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 
