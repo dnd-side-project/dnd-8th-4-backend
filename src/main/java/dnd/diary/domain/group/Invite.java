f9c4c0de58ce8a09419f34d72381809dce3247b4
simmigyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-21 16:38:54 +0900

    [Feat] 그룹 초대, 알림 관련 클래스 필드 추가 및 기본 메서드 생성

--- src/main/java/dnd/diary/domain/group/Invite.java
+++ src/main/java/dnd/diary/domain/group/Invite.java
@@ -3,6 +3,7 @@ package dnd.diary.domain.group;
 import dnd.diary.domain.BaseEntity;
 import dnd.diary.domain.user.User;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,15 +21,36 @@ public class Invite extends BaseEntity {
     @Column(name = "invite_id")
     private Long id;
 
+    // 어느 그룹의 초대인지
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
 
+    // 누가 초대 받았는지
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
+    // 초대에 대한 행동(수락/거절)을 했는지 여부 - 수락/거절 api 호출 후 true 로
+    private boolean processYn;
+
     // 초대에 대한 알림 목록
     @OneToMany(mappedBy = "invite")
     private List<Notification> notifications = new ArrayList<>();
+
+    @Builder
+    private Invite(Group group, User user) {
+        this.group = group;
+        this.user = user;
+        this.processYn = false;
+    }
+
+    public static Invite toEntity(Group group, User user) {
+        return new Invite(group, user);
+    }
+
+    // 초대에 대한 수락/거절 처리 상태 변경
+    public void updateProcessUYn() {
+        this.processYn = true;
+    }
 }
--- src/main/java/dnd/diary/domain/group/Notification.java
+++ src/main/java/dnd/diary/domain/group/Notification.java
@@ -2,6 +2,7 @@ package dnd.diary.domain.group;
 
 import dnd.diary.domain.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +21,25 @@ public class Notification extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "invite_id")
     private Invite invite;
+
+    // 알림을 읽었는지 여부
+    private boolean readYn;
+
+    @Builder
+    private Notification(Invite invite) {
+        this.invite = invite;
+        this.readYn = false;
+
+        invite.getNotifications().add(this);
+    }
+
+    public static Notification toEntity(Invite invite) {
+        return new Notification(invite);
+    }
+
+    // 알림 읽음 처리
+    public void readNotification() {
+        this.readYn = true;
+    }
+
 }
