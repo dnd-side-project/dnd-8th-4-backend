50bf44e18e2f73d8f07a3fb7786dbf1e45488ca3
Hyun Woo <hwsa10041@gmail.com>
2023-02-21 18:46:04 +0900

    [FEAT, CHORE] 북마크한 글 조회 API 구현
    - ContentDto에서 사용되지 않던 클래스 삭제

--- src/main/java/dnd/diary/repository/content/BookmarkRepository.java
+++ src/main/java/dnd/diary/repository/content/BookmarkRepository.java
@@ -1,9 +1,14 @@
 package dnd.diary.repository.content;
 
 import dnd.diary.domain.bookmark.Bookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    Page<Bookmark> findByUserId(Long userId, Pageable pageable);
 }
