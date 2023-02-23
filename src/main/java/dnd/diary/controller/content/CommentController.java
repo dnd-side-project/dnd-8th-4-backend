10191166307883630f5162b6b87f211eae99e387
Hyun Woo <hwsa10041@gmail.com>
2023-02-23 20:56:54 +0900

    [FEAT] 피드 공감 리스트 조회 API 추가
    - 댓글 조회 API에서 분리

--- src/main/java/dnd/diary/controller/content/CommentController.java
+++ src/main/java/dnd/diary/controller/content/CommentController.java
@@ -1,14 +1,17 @@
 package dnd.diary.controller.content;
 
 import dnd.diary.dto.content.CommentDto;
+import dnd.diary.dto.content.ContentDto;
 import dnd.diary.response.CustomResponseEntity;
 import dnd.diary.service.content.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,11 +30,19 @@ public class CommentController {
 
     // 피드 댓글 조회
     @GetMapping("content/{contentId}/comment")
-    public CustomResponseEntity<CommentDto.pagePostsCommentDto> pageComment(
+    public CustomResponseEntity<Page<CommentDto.pageCommentDto>> pageComment(
             @AuthenticationPrincipal final UserDetails userDetails,
             @PathVariable(name = "contentId") final Long contentId,
             @RequestParam final Integer page
     ) {
         return commentService.commentPage(userDetails, contentId, page);
+    }
+
+    // 피드 감정 리스트 조회
+    @GetMapping("content/{contentId}/emotion")
+    public CustomResponseEntity<List<ContentDto.EmotionResponseDto>> listEmotion(
+            @PathVariable(name = "contentId") final Long contentId
+    ){
+        return commentService.emotionList(contentId);
     }
 }
--- src/main/java/dnd/diary/dto/content/CommentDto.java
+++ src/main/java/dnd/diary/dto/content/CommentDto.java
@@ -2,19 +2,14 @@ package dnd.diary.dto.content;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dnd.diary.domain.comment.Comment;
-import dnd.diary.domain.comment.Sticker;
-import dnd.diary.domain.content.Content;
-import dnd.diary.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
-import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class CommentDto {
 
@@ -32,7 +27,7 @@ public class CommentDto {
         private Long contentId;
         private Long stickerId;
 
-        public static CommentDto.AddCommentDto response(Comment comment){
+        public static CommentDto.AddCommentDto response(Comment comment) {
             return AddCommentDto.builder()
                     .id(comment.getId())
                     .commentNote(comment.getCommentNote())
@@ -52,39 +47,19 @@ public class CommentDto {
 
         private Long id;
         private String commentNote;
+        private String profileImageUrl;
         private String username;
         private LocalDateTime createdAt;
         private Boolean likesExists;
 
-        public static CommentDto.pageCommentDto response(Comment comment, Boolean likesExists){
+        public static CommentDto.pageCommentDto response(Comment comment, Boolean likesExists) {
             return pageCommentDto.builder()
                     .id(comment.getId())
                     .commentNote(comment.getCommentNote())
+                    .profileImageUrl(comment.getUser().getProfileImageUrl())
                     .username(comment.getUser().getNickName())
                     .createdAt(comment.getCreatedAt())
                     .likesExists(likesExists)
-                    .build();
-        }
-    }
-
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @Getter
-    @Builder
-    public static class pagePostsCommentDto {
-
-        private Long emotions;
-        private Long comments;
-        private List<ContentDto.EmotionResponseDto> emotionList;
-        private Page<pageCommentDto> page;
-
-        public static CommentDto.pagePostsCommentDto response(Page<pageCommentDto> page, List<ContentDto.EmotionResponseDto> emotionList, Long emotions, Long comments){
-            return pagePostsCommentDto.builder()
-                    .emotions(emotions)
-                    .comments(comments)
-                    .emotionList(emotionList)
-                    .page(page)
                     .build();
         }
     }
