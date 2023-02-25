1133a3cd715cee4be53ce4922a61732d6a996345
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-02-26 04:19:08 +0900

    [Feat] 그룹 초대 수락/거절 API 구현 변경
    [테스트 시나리오]
    1. A 가 그룹 1 을 생성
    2. A 가 그룹 1 에 대한 미션 1 을 생성
    3. A 가 B 를 그룹 1에 초대
    4. B 가 (그룹 1 에 대한) 초대 알림 조회
    5. B 가 그룹 1 에 대한 초대 수락
    6. B 가 가진 미션 목록 조회 - 미션 1 이 존재하는지 확인

--- src/main/java/dnd/diary/service/group/InviteService.java
+++ src/main/java/dnd/diary/service/group/InviteService.java
@@ -4,6 +4,12 @@ import static dnd.diary.enumeration.Result.*;
 
 import java.util.List;
 
+import dnd.diary.domain.group.Notification;
+import dnd.diary.domain.mission.Mission;
+import dnd.diary.domain.mission.UserAssignMission;
+import dnd.diary.repository.group.UserJoinGroupRepository;
+import dnd.diary.repository.mission.UserAssignMissionRepository;
+import dnd.diary.response.notification.NotificationResponse;
 import org.springframework.stereotype.Service;
 
 import dnd.diary.domain.group.Group;
@@ -19,6 +25,7 @@ import dnd.diary.repository.user.UserRepository;
 import dnd.diary.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,29 +33,75 @@ import lombok.extern.slf4j.Slf4j;
 public class InviteService {
 
 	private final GroupRepository groupRepository;
-	private final InviteRepository inviteRepository;
 	private final NotificationRepository notificationRepository;
 	private final UserRepository userRepository;
+	private final UserJoinGroupRepository userJoinGroupRepository;
+	private final UserAssignMissionRepository userAssignMissionRepository;
 
 	private final UserService userService;
 
 	// 초대 수락
-	public void acceptInvite(Long groupId, Long notificationId, boolean acceptYn) {
+	@Transactional
+	public NotificationResponse.NotificationInfo acceptInvite(Long groupId, Long notificationId) {
 		User user = findUser();
 		Group invitedGroup = findGroup(groupId);
 
-		checkAlreadyExist(user, invitedGroup);
+		checkAlreadyExist(user, invitedGroup);   //
 
+		// 그룹 가입 처리
+		UserJoinGroup userJoinGroup = UserJoinGroup.toEntity(user, invitedGroup);
+		userJoinGroupRepository.save(userJoinGroup);
 
+		/**
+		 * 이미 그룹에 존재하는 미션 할당 처리
+		 */
+		for (Mission mission : invitedGroup.getMissions()) {
+			UserAssignMission addUserAssignMission = UserAssignMission.toEntity(user, mission);
+			userAssignMissionRepository.save(addUserAssignMission);
+			log.info("그룹 가입 수락으로 추가될 미션 ID : {}", mission.getId());
+			log.info("그룹 가입 수락으로 추가될 할당된 addUserAssignMission ID : {}", addUserAssignMission.getId());
+		}
+
+		// 알림 읽음 처리
+		Notification notification = findNotification(notificationId);
+		notification.readNotification();
+
+		NotificationResponse.NotificationInfo notificationInfo = NotificationResponse.NotificationInfo.builder()
+				.notificationId(notification.getId())
+				.groupId(invitedGroup.getId())
+				.groupName(invitedGroup.getGroupName())
+				.groupNote(invitedGroup.getGroupNote())
+				.groupImageUrl(invitedGroup.getGroupImageUrl())
+				.groupInvitedAt(notification.getCreatedAt())
+				.readYn(notification.isReadYn())
+				.build();
+
+		return notificationInfo;
 	}
 
-
 	// 초대 거절
-	public void rejectInvite(Long groupId, Long notificationId, boolean acceptYn) {
+	@Transactional
+	public NotificationResponse.NotificationInfo rejectInvite(Long groupId, Long notificationId, boolean acceptYn) {
 		User user = findUser();
 		Group invitedGroup = findGroup(groupId);
 
 		checkAlreadyExist(user, invitedGroup);
+
+		// 알림 읽음 처리
+		Notification notification = findNotification(notificationId);
+		notification.readNotification();
+
+		NotificationResponse.NotificationInfo notificationInfo = NotificationResponse.NotificationInfo.builder()
+				.notificationId(notification.getId())
+				.groupId(invitedGroup.getId())
+				.groupName(invitedGroup.getGroupName())
+				.groupNote(invitedGroup.getGroupNote())
+				.groupImageUrl(invitedGroup.getGroupImageUrl())
+				.groupInvitedAt(notification.getCreatedAt())
+				.readYn(notification.isReadYn())
+				.build();
+
+		return notificationInfo;
 	}
 
 	// 이미 가입한 그룹인지 체크
@@ -68,5 +121,9 @@ public class InviteService {
 	private User findUser() {
 		UserDto.InfoDto userInfo = userService.findMyListUser();
 		return userRepository.findById(userInfo.getId()).orElseThrow(() -> new CustomException(NOT_FOUND_USER));
+	}
+
+	private Notification findNotification(Long notificationId) {
+		return notificationRepository.findById(notificationId).orElseThrow(() -> new CustomException(NOT_FOUND_NOTIFICATION));
 	}
 }
