bbafece98246b11251808923acb34695df4b842c
Hyun Woo <hwsa10041@gmail.com>
2023-02-28 15:30:20 +0900

    [FEAT] 피드 상세 보기 성능 개선
    - Redis Look Aside + Write Around 전략

--- src/main/java/dnd/diary/service/content/ContentService.java
+++ src/main/java/dnd/diary/service/content/ContentService.java
@@ -29,6 +29,7 @@ import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -170,6 +171,7 @@ public class ContentService {
     }
 
     @Transactional
+    @CacheEvict(value = "Contents", key = "#contentId", cacheManager = "testCacheManager")
     public ContentDto.UpdateDto updateContent(
             UserDetails userDetails, List<MultipartFile> multipartFile, Long contentId,
             String contentNote, Double latitude, Double longitude, String location
