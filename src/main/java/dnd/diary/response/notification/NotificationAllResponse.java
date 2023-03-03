76f74763d5997e7595205ff70c3268238e824e62
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-03-03 23:09:49 +0900

    [Fix] 전체 알림 목록 조회 API response 누락값 수정 및 최신순 정렬

--- src/main/java/dnd/diary/response/notification/NotificationAllResponse.java
+++ src/main/java/dnd/diary/response/notification/NotificationAllResponse.java
@@ -5,10 +5,8 @@ import dnd.diary.domain.comment.Comment;
 import dnd.diary.domain.content.Content;
 import dnd.diary.domain.content.Emotion;
 import dnd.diary.domain.group.Group;
-import dnd.diary.domain.group.Invite;
 import dnd.diary.domain.group.Notification;
 import dnd.diary.domain.group.NotificationType;
-import dnd.diary.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -44,6 +42,7 @@ public class NotificationAllResponse {
         private Long contentId;
         private Long commentId;
         private String commentNote;
+        private Long stickerId;
 
         // 게시물 공감 관련
         private Long emotionId;
@@ -67,15 +66,15 @@ public class NotificationAllResponse {
             this.groupNote = invitedGroup.getGroupNote();
             this.groupImageUrl = invitedGroup.getGroupImageUrl();
 
-            this.userName = notification.getUser().getName();
-            this.userProfileImageUrl = notification.getUser().getProfileImageUrl();
+            // 초대한 유저 기준
+            this.userName = invitedGroup.getGroupCreateUser().getName();
+            this.userProfileImageUrl = invitedGroup.getGroupCreateUser().getProfileImageUrl();
 
             this.createdAt = notification.getCreatedAt();
             this.readYn = notification.isReadYn();
-
         }
 
-        // 게시물 댓글 response
+        // 내 게시물 댓글 response
         @Builder
         public NotificationInfo(Notification notification, Content content, Comment comment) {
             this.notificationId = notification.getId();
@@ -87,34 +86,39 @@ public class NotificationAllResponse {
             this.groupNote = targetGroup.getGroupNote();
             this.groupImageUrl = targetGroup.getGroupImageUrl();
 
-            this.userName = notification.getUser().getName();
-            this.userProfileImageUrl = notification.getUser().getProfileImageUrl();
+            this.userName = comment.getUser().getName();
+            this.userProfileImageUrl = comment.getUser().getProfileImageUrl();
+
+            this.contentId = content.getId();
+            this.commentId = comment.getId();
+            this.commentNote = comment.getCommentNote();
+            this.stickerId = comment.getSticker().getId();
 
             this.createdAt = notification.getCreatedAt();
             this.readYn = notification.isReadYn();
-
         }
 
-        // 게시물 공감 response
+        // 내 게시물 공감 response
         @Builder
         public NotificationInfo(Notification notification, Content content, Emotion emotion) {
             this.notificationId = notification.getId();
             this.notificationType = notification.getNotificationType();
 
-            Group invitedGroup = notification.getInvite().getGroup();
+            Group invitedGroup = notification.getContent().getGroup();
             this.groupId = invitedGroup.getId();
             this.groupName = invitedGroup.getGroupName();
             this.groupNote = invitedGroup.getGroupNote();
             this.groupImageUrl = invitedGroup.getGroupImageUrl();
 
-            this.userName = notification.getUser().getName();
-            this.userProfileImageUrl = notification.getUser().getProfileImageUrl();
+            this.userName = emotion.getUser().getName();
+            this.userProfileImageUrl = emotion.getUser().getProfileImageUrl();
+
+            this.emotionId = emotion.getId();
+            this.emotionStatus = emotion.getEmotionStatus();
 
             this.createdAt = notification.getCreatedAt();
             this.readYn = notification.isReadYn();
-
         }
-
     }
 
     private long totalCount;
