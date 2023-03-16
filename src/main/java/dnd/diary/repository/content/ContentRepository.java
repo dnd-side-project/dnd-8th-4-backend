4a64c5f9311b7477282e44ff123139041469e682
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-03-17 03:38:28 +0900

    [Refactor] 수정 전 코드 주석 제거

--- src/main/java/dnd/diary/repository/content/ContentRepository.java
+++ src/main/java/dnd/diary/repository/content/ContentRepository.java
@@ -11,15 +11,6 @@ import java.util.Optional;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
-//    Optional<Content> findByIdAndUserId(Long contentId, Long userId);
-//    Page<Content> findByGroupId(Long groupId, Pageable pageable);
-//    Page<Content> findByGroupIdIn(List<Long> groupId, Pageable pageable);
-//    List<Content> findByIdIn(List<Long> contentId);
-//    Page<Content> findByIdIn(List<Long> contentId, Pageable pageable);
-//    Page<Content> findByUserId(Long userId, Pageable pageable);
-//    Page<Content> findByContentContainingAndGroupIdIn(String word, List<Long> groupId, Pageable pageable);
-//    List<Content> findByLocationAndGroupIdIn(String location, List<Long> groupId);
-//    Long countByLocationAndGroupIdIn(String location, List<Long> groupId);
 
     Optional<Content> findByIdAndUserIdAndDeletedYn(Long contentId, Long userId, Boolean deleteYn);
     Page<Content> findByGroupIdAndDeletedYn(Long groupId, Boolean deleteYn, Pageable pageable);
