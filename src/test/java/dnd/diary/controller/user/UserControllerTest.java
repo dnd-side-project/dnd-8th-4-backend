34050fcd6b1def833f8a2a2bf0257fc35d4ee59a
Hyun Woo <hwsa10041@gmail.com>
2023-05-19 19:55:37 +0900

    [FEAT] 유저 정보 수정, 로그아웃 컨트롤러 테스트 작성

--- src/test/java/dnd/diary/controller/user/UserControllerTest.java
+++ src/test/java/dnd/diary/controller/user/UserControllerTest.java
@@ -5,8 +5,12 @@ import dnd.diary.controller.ControllerTestSupport;
 import dnd.diary.request.controller.user.UserRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.net.URI;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,6 +55,39 @@ class UserControllerTest extends ControllerTestSupport {
         // when // then
         mockMvc.perform(
                         MockMvcRequestBuilders.get("/auth/my/info")
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("유저 정보 수정 API")
+    @Test
+    void userUpdateProfile() throws Exception {
+        // given
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "hello.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "Hello, World!".getBytes()
+        );
+
+        // when // then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.multipart(HttpMethod.PATCH, URI.create("/auth"))
+                                .file(multipartFile)
+                                .param("nickName","test")
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("유저 로그아웃 API")
+    @Test
+    void logoutUser() throws Exception {
+        // when // then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/auth/logout")
+                                .header("Authorization","testToken")
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
