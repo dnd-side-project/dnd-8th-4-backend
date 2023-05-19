c54dc9ba8488ee739001d533682c016f98e8c3a2
Hyun Woo <hwsa10041@gmail.com>
2023-05-19 17:49:43 +0900

    [FEAT] @EnableJpaAuditing 분리

--- src/main/java/dnd/diary/DiaryApplication.java
+++ src/main/java/dnd/diary/DiaryApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
 @EnableCaching
 @SpringBootApplication
 public class DiaryApplication {
